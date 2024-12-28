de28d5426d32c906ab521ff651d811b7344ca484
Aman Prakash <amanjhasahab@gmail.com>
2024-12-29 00:10:54 +0530

    setMatrixToZero LC

--- src/Arrays/SetMatrixToZero.java
+++ src/Arrays/SetMatrixToZero.java
@@ -31,31 +31,31 @@ public class SetMatrixToZero {
             }
         }
 
-//
-//        for(int i=1; i<matrix.length;i++)
-//        {
-//            for (int j=1;j<matrix[0].length;j++)
-//            {
-//                if(matrix[0][j]==0 || matrix[i][0]==0)
-//                {
-//                    matrix[i][j]=0;
-//                }
-//            }
-//        }
-//
-//        if(firstRow)
-//        {
-//            for(int i=0; i<matrix[0].length; i++)
-//            {
-//                matrix[0][i]=0;
-//            }
-//        }
-//
-//        if(firstCol)
-//            for(int i=0; i<matrix.length; i++)
-//            {
-//                matrix[i][0]=0;
-//            }
+
+        for(int i=1; i<matrix.length;i++)
+        {
+            for (int j=1;j<matrix[0].length;j++)
+            {
+                if(matrix[0][j]==0 || matrix[i][0]==0)
+                {
+                    matrix[i][j]=0;
+                }
+            }
+        }
+
+        if(firstRow)
+        {
+            for(int i=0; i<matrix[0].length; i++)
+            {
+                matrix[0][i]=0;
+            }
+        }
+
+        if(firstCol)
+            for(int i=0; i<matrix.length; i++)
+            {
+                matrix[i][0]=0;
+            }
 
 
         for(int i=0; i<matrix.length; i++)
